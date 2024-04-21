73609b9a779d3aae02f65ba7a567de5eef93770b
nene97 <nebojsavelkovski@live.com>
2024-04-21 16:59:03 +0200

    scrollBtnAdding

--- pageTwo/index.js
+++ pageTwo/index.js
@@ -6,3 +6,31 @@ window.onscroll = function() {
     nav.classList.remove("sticky");
   }
 }
+
+
+
+
+let mybutton = document.getElementById("btnToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
